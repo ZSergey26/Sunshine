0ec461eedf43e6267da95780e2a11ba9cf4edff1
Sergey Zarochentsev <zsergey26@gmail.com>
2015-07-12 21:58:23 +0300

    HttpUrl connection code

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,13 +25,15 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
+    private final static String TAG = "Sunshine";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -54,9 +63,9 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class ForecastFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public ForecastFragment() {
         }
 
         @Override
@@ -71,12 +80,72 @@ public class MainActivity extends ActionBarActivity {
                     "Thurs - Sunny - 14/88",
                     "Fri - Sunny - 14/88",
                     "Sat - Sunny - 14/88",
+                    "Sun - Sunny - 14/88",
+                    "Today - Sunny - 12/32",
+                    "Tomorrow - Foggy - 13/28",
+                    "Weds - Cloudy - 14/88",
+                    "Thurs - Sunny - 14/88",
+                    "Fri - Sunny - 14/88",
+                    "Sat - Sunny - 14/88",
                     "Sun - Sunny - 14/88"
             };
             List<String > weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast );
             ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
             listView.setAdapter(adapter);
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String forecastJsonString = null;
+
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?q=94043,usa&mode=json&units=metric&cnt=7");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(inputStream == null)
+                {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+
+                String line;
+                while((line=reader.readLine()) != null)
+                {
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length() == 0)
+                {
+                    return null;
+                }
+
+                forecastJsonString = buffer.toString();
+            }
+            catch (IOException e) {
+                Log.e(TAG, " IOException!", e);
+            }
+            finally {
+                if(urlConnection != null)
+                {
+                    urlConnection.disconnect();
+                }
+
+                if(reader!=null)
+                {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
 
             return rootView;
         }
