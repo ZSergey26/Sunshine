8660d2d866c93ec4286f035c2af0b3cab34c6df6
Sergey Zarochentsev <zsergey26@gmail.com>
2015-07-20 23:29:22 +0300

    fix

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -262,11 +262,7 @@ public class ForecastFragment extends Fragment {
             highAndLow = formatHighLows(high, low);
             resultStrs[i] = day + " - " + description + " - " + highAndLow;
         }
-        System.out.println("kek");
 
-        for (String s : resultStrs) {
-            Log.v(LOG_TAG, "Forecast entry: " + s);
-        }
         return resultStrs;
 
     }
